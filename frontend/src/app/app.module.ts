bba27f125d958d4c094fe98d95a09b99f9cc3c33
Ignacio Cuevas <ignacio.cuevas.s@mail.pucv.cl>
2025-04-25 01:24:14 -0400

    Actualización Quienes Somos (página)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
